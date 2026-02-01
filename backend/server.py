ec6e6e3f235c28d6cc963e25881c0f5d68eee74c
Manish Pandey <rockmanish90@gmail.com>
2026-02-01 17:07:58 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -22,7 +22,7 @@ ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
 # MongoDB connection
-mongo_url = os.environ['MONGO_URL']
+mongo_url = os.environ.get('MONGO_URL')
 client = AsyncIOMotorClient(mongo_url)
 db = client[os.environ['DB_NAME']]
 
